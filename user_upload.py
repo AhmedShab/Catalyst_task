892695d4d4e0dd564ca05fb77a4fc6e79a6cfc2e
AhmedShab <ahmed.vuw@gmail.com>
2017-08-04 20:38:36 +1200

    created the required table (users). Also using optparse module as a better solution than sys.argv (can also pass username, password and host via terminal)

--- user_upload.py
+++ user_upload.py
@@ -1,15 +1,53 @@
 import sys
 import csv
 import MySQLdb
+import optparse
 
 create_table = "--create_table"
 upload_file = "--file"
 
-if sys.argv[1] == upload_file:
-    with open(sys.argv[2], 'rb') as csvfile:
+parser = optparse.OptionParser()
+
+parser.add_option('--file',
+                  action="store", type="string",
+                  dest="file",help="require the csv file to insert the data into the users table")
+
+parser.add_option('--create_table',
+                  action="store_true", dest="create_table",
+                  help="This will build/rebuild a new table")
+
+parser.add_option('-u',
+                  action="store", dest="username",
+                  help="MySQL username")
+
+parser.add_option('-p',
+               action="store", dest="password",
+               help="MySQL password")
+
+parser.add_option('--host',
+                  action="store", dest="host",
+                  help="MySQL host")
+
+(options, args) = parser.parse_args()
+
+db = MySQLdb.connect(host=options.host, user=options.username, passwd=options.password, db="record")
+
+cursor = db.cursor()
+
+if options.file:
+    with open(options.file, 'rb') as csvfile:
         filename = csv.reader(csvfile)
         for row in filename:
             print(row)
 
-if sys.argv[1] == create_table:
-    print(create_table)
+elif options.create_table:
+        cursor.execute("drop table if exists users")
+        sql = """create table users(
+                name char(20),
+                surname char(20),
+                email char(30),
+                unique (email)
+                );"""
+        cursor.execute(sql)
+
+db.close()
