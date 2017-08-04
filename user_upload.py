36fc88a7a4e1dbfeb43719cdece3b90b20fa29fe
AhmedShab <ahmed.vuw@gmail.com>
2017-08-05 06:03:01 +1200

    handling invalid emails. If found, don't insert them and report them

--- user_upload.py
+++ user_upload.py
@@ -2,9 +2,16 @@ import sys
 import csv
 import MySQLdb
 import optparse
+import re
 
 def insert_into_table(user_data):
     return None
+
+def remove_invalidate_emails(data):
+    is_valid = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    if (is_valid.search(data["email"]) == None):
+        del data["email"]
+        return None
 
 create_table = "--create_table"
 upload_file = "--file"
@@ -38,12 +45,20 @@ db = MySQLdb.connect(host=options.host, user=options.username, passwd=options.pa
 cursor = db.cursor()
 
 if options.file:
-    with open(options.file, 'rb') as csvfile:
-        data = csv.DictReader(csvfile)
-        for row in data:
-            row["email"] = row.pop("email\t")
+	with open(options.file, 'rb') as csvfile:
+		data = csv.DictReader(csvfile)
+		for row in data:
+			try:
+				row["email"] = row.pop("email\t")
+				remove_invalidate_emails(row)
+				# call insert_into_table with a valid email
+				# insert_into_table(row)
+				print(row["name"], row["surname"], row["email"])
 
-            print(row["name"], row["surname"], row["email"])
+			except Exception as e:
+				print "the email is invalid! Please use a valid email"
+				# no insertion happens
+				continue
 
 elif options.create_table:
         cursor.execute("drop table if exists users")
