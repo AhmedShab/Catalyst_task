5fcc0a28ab940414a2aafc8fbe32338cce6473cd
AhmedShab <ahmed.vuw@gmail.com>
2017-08-04 20:06:18 +1200

    added simple solution to add different command for the args (for now)

--- user_upload.py
+++ user_upload.py
@@ -2,9 +2,14 @@ import sys
 import csv
 import MySQLdb
 
+create_table = "--create_table"
+upload_file = "--file"
 
+if sys.argv[1] == upload_file:
+    with open(sys.argv[2], 'rb') as csvfile:
+        filename = csv.reader(csvfile)
+        for row in filename:
+            print(row)
 
-with open(sys.argv[1], 'rb') as csvfile:
-    filename = csv.reader(csvfile)
-    for row in filename:
-        print(row)
+if sys.argv[1] == create_table:
+    print(create_table)
