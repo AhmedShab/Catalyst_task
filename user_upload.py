3a98718b21d15afca9b0d0798a42d81c37af38eb
AhmedShab <ahmed.vuw@gmail.com>
2017-08-05 07:34:32 +1200

    wrapped everything (outer code) in the main function

--- user_upload.py
+++ user_upload.py
@@ -25,60 +25,63 @@ def contains_non_alpha_chars(fullname):
 	regex = re.compile("[.,\/#!$%\^&\*;:{}=\-_`~()]")
 	return regex.search(fullname["name"]) is not None or regex.search(fullname["surname"]) is not None
 
-create_table = "--create_table"
-upload_file = "--file"
+def main():
+	create_table = "--create_table"
+	upload_file = "--file"
 
-parser = optparse.OptionParser()
+	parser = optparse.OptionParser()
 
-parser.add_option('--file',
-                  action="store", type="string",
-                  dest="file",help="require the csv file to insert the data into the users table")
+	parser.add_option('--file',
+	action="store", type="string",
+	dest="file",help="require the csv file to insert the data into the users table")
 
-parser.add_option('--create_table',
-                  action="store_true", dest="create_table",
-                  help="This will build/rebuild a new table")
+	parser.add_option('--create_table',
+	action="store_true", dest="create_table",
+	help="This will build/rebuild a new table")
 
-parser.add_option('-u',
-                  action="store", dest="username",
-                  help="MySQL username")
+	parser.add_option('-u',
+	action="store", dest="username",
+	help="MySQL username")
 
-parser.add_option('-p',
-               action="store", dest="password",
-               help="MySQL password")
+	parser.add_option('-p',
+	action="store", dest="password",
+	help="MySQL password")
 
-parser.add_option('--host',
-                  action="store", dest="host",
-                  help="MySQL host")
+	parser.add_option('--host',
+	action="store", dest="host",
+	help="MySQL host")
 
-(options, args) = parser.parse_args()
+	(options, args) = parser.parse_args()
 
-db = MySQLdb.connect(host=options.host, user=options.username, passwd=options.password, db="record")
+	db = MySQLdb.connect(host=options.host, user=options.username, passwd=options.password, db="record")
 
-cursor = db.cursor()
+	cursor = db.cursor()
 
-if options.file:
-	with open(options.file, 'rb') as csvfile:
-		data = csv.DictReader(csvfile)
-		for row in data:
-			try:
-				remove_invalid_email(row)
-				fix_fullname_format(row)
-				# insert_into_table(row)
-				print(row["name"], row["surname"], row["email"])
+	if options.file:
+		with open(options.file, 'rb') as csvfile:
+			data = csv.DictReader(csvfile)
+			for row in data:
+				try:
+					remove_invalid_email(row)
+					fix_fullname_format(row)
+					# insert_into_table(row)
+					print(row["name"], row["surname"], row["email"])
 
-			except KeyError as e:
-				print "the email is invalid! Please use a valid email"
-				# no insertion happens
-				continue
+				except KeyError as e:
+					print "the email is invalid! Please use a valid email"
+					# no insertion happens
+					continue
 
-elif options.create_table:
-        cursor.execute("drop table if exists users")
-        sql = """create table users(
-                name char(20),
-                surname char(20),
-                email char(30),
-                unique (email)
-                );"""
-        cursor.execute(sql)
+	elif options.create_table:
+		cursor.execute("drop table if exists users")
+		sql = """create table users(
+				name char(20),
+				surname char(20),
+				email char(30),
+				unique (email)
+				);"""
+		cursor.execute(sql)
 
-db.close()
+	db.close()
+
+main()	
