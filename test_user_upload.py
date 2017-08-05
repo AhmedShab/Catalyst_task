af7eef1c50fe091f2289a84438833327f3fd3369
AhmedShab <ahmed.vuw@gmail.com>
2017-08-06 08:29:36 +1200

    testing surnames with no quote, should return false

--- test_user_upload.py
+++ test_user_upload.py
@@ -1,5 +1,6 @@
 from user_upload import append_quote_to_names
 from user_upload import contains_single_quote
+from user_upload import remove_invalid_email
 
 
 def test_surname_contains_quote__append_quote():
@@ -7,9 +8,8 @@ def test_surname_contains_quote__append_quote():
     test = "A'hmed"
     assert append_quote_to_names(test, "'") == expacted
 
-def test_surname_contains_no_quote__does_not_append_quote():
-    expacted = "Ahmed"
+def test_surname_contains_no_quote__return_false():
+    expacted = False
     test = "Ahmed"
 
-    if contains_single_quote(test, "'"):
-        assert append_quote_to_names(test, "'") == expacted
+    assert contains_single_quote(test, "'") == expacted
