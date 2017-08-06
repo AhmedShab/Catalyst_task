b1e6bd680303e0a956614e64a336b79c85aff6c7
AhmedShab <ahmed.vuw@gmail.com>
2017-08-06 12:09:47 +1200

    importing everything from user_upload script instead of importing individual function

--- test_user_upload.py
+++ test_user_upload.py
@@ -1,39 +1,35 @@
-from user_upload import append_quote_to_names
-from user_upload import contains_single_quote
-from user_upload import remove_invalid_email
-from user_upload import is_invalid_email
-from user_upload import fix_fullname_format
+import user_upload
 
 
 def test_surname_contains_quote__append_quote():
     expacted = "A''hmed"
     test = "A'hmed"
-    assert append_quote_to_names(test, "'") == expacted
+    assert user_upload.append_quote_to_names(test, "'") == expacted
 
 def test_surname_contains_no_quote__return_false():
     expacted = False
     test = "Ahmed"
 
-    assert contains_single_quote(test, "'") == expacted
+    assert user_upload.contains_single_quote(test, "'") == expacted
 
 def test_invalid_email__return_true():
     expacted = True
     test = "ahmed&&&@gmail.com"
 
-    assert is_invalid_email(test) == expacted
+    assert user_upload.is_invalid_email(test) == expacted
 
 def test_valid_email__return_false():
     expacted = False
     test = "ahmed@gmail.com"
 
-    assert is_invalid_email(test) == expacted
+    assert user_upload.is_invalid_email(test) == expacted
 
 def test_remove_invalid_email__should_remove_email():
     expacted = None
     test = {"email": "ahmed&&&@gmail.com"}
 
     try:
-        test = remove_invalid_email(test)
+        test = user_upload.remove_invalid_email(test)
 
     except KeyError as e:
         print "Eamil should be removed, thus, key does not exists"
@@ -44,12 +40,12 @@ def test_remove_invalid_email__should_not_remove_email():
     expacted = "ahmed@gmail.com"
     test = {"email": "ahmed@gmail.com"}
 
-    assert remove_invalid_email(test) == expacted
+    assert user_upload.remove_invalid_email(test) == expacted
 
 def test_fix_fullname_format__should_return_fixed_text():
     expacted = {"name": "Ahmed", "surname": "Shaaban"}
     test = {"name": "ahmed?", "surname": "shaaban***?"}
 
-    fix_fullname_format(test)
+    user_upload.fix_fullname_format(test)
 
     assert test == expacted
