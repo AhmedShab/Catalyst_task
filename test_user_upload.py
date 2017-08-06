6f6f6703a3159d36c00ec132a2e3201affdc8309
AhmedShab <ahmed.vuw@gmail.com>
2017-08-06 15:58:12 +1200

    using unittest instead of nose

--- test_user_upload.py
+++ test_user_upload.py
@@ -1,66 +1,71 @@
 import user_upload
+import unittest
+from mock import MagicMock
+
+class TestUserUpload(unittest.TestCase):
+
+    def test_surname_contains_quote__append_quote(self):
+        expacted = "A''hmed"
+        test = "A'hmed"
+        self.assertTrue(user_upload.append_quote_to_names(test, "'"), expacted)
+
+    def test_surname_contains_no_quote__return_false(self):
+        expacted = False
+        test = "Ahmed"
+
+        self.assertFalse(user_upload.contains_single_quote(test, "'"), expacted)
+
+    def test_invalid_email__return_true(self):
+        expacted = True
+        test = "ahmed&&&@gmail.com"
+
+        self.assertTrue(user_upload.is_invalid_email(test), expacted)
+
+    def test_valid_email__return_false(self):
+        expacted = False
+        test = "ahmed@gmail.com"
+
+        self.assertFalse(user_upload.is_invalid_email(test), expacted)
+
+    def test_remove_invalid_email__should_remove_email(self):
+        expacted = None
+        test = {"email": "ahmed&&&@gmail.com"}
+
+        try:
+            user_upload.remove_invalid_email(test)
+
+        except KeyError as e:
+            print "Eamil should be removed, thus, throw an error since the key does not exists"
 
 
-def test_surname_contains_quote__append_quote():
-    expacted = "A''hmed"
-    test = "A'hmed"
-    assert user_upload.append_quote_to_names(test, "'") == expacted
+    def test_remove_invalid_email__should_not_remove_email(self):
+        expacted = "ahmed@gmail.com"
+        test = {"email": "ahmed@gmail.com"}
 
-def test_surname_contains_no_quote__return_false():
-    expacted = False
-    test = "Ahmed"
+        self.assertTrue(user_upload.remove_invalid_email(test), expacted)
 
-    assert user_upload.contains_single_quote(test, "'") == expacted
+    def test_fix_fullname_format__return_fixed_text(self):
+        expacted = {"name": "Ahmed", "surname": "Shaaban"}
+        test = {"name": "ahmed?", "surname": "shaaban***?"}
 
-def test_invalid_email__return_true():
-    expacted = True
-    test = "ahmed&&&@gmail.com"
+        user_upload.fix_fullname_format(test)
 
-    assert user_upload.is_invalid_email(test) == expacted
+        self.assertTrue(test, expacted)
 
-def test_valid_email__return_false():
-    expacted = False
-    test = "ahmed@gmail.com"
+    def test_clean_whitespaces__should_remove_whitespaces(self):
+        expacted = {"name": "ahmed", "surname": "shaaban", "email": "ahmed@gmail.com"}
+        test = {"name": "ahmed   ", "surname": "   shaaban", "email": "      ahmed@gmail.com   "}
 
-    assert user_upload.is_invalid_email(test) == expacted
+        user_upload.remove_whitespaces(test)
 
-def test_remove_invalid_email__should_remove_email():
-    expacted = None
-    test = {"email": "ahmed&&&@gmail.com"}
-
-    try:
-        test = user_upload.remove_invalid_email(test)
-
-    except KeyError as e:
-        print "Eamil should be removed, thus, key does not exists"
-        test == expacted
+        self.assertTrue(test, expacted)
 
 
-def test_remove_invalid_email__should_not_remove_email():
-    expacted = "ahmed@gmail.com"
-    test = {"email": "ahmed@gmail.com"}
+    def test_set_email_lower_case__return_lower_case_email(self):
+        expacted = "ahmed@gmail.com"
+        test = {"email": "AHMED@gmaIL.com"}
 
-    assert user_upload.remove_invalid_email(test) == expacted
+        self.assertTrue(user_upload.set_email_lower_case(test), expacted)    
 
-def test_fix_fullname_format__return_fixed_text():
-    expacted = {"name": "Ahmed", "surname": "Shaaban"}
-    test = {"name": "ahmed?", "surname": "shaaban***?"}
-
-    user_upload.fix_fullname_format(test)
-
-    assert test == expacted
-
-def test_clean_whitespaces__should_remove_whitespaces():
-    expacted = {"name": "ahmed", "surname": "shaaban", "email": "ahmed@gmail.com"}
-    test = {"name": "ahmed   ", "surname": "   shaaban", "email": "      ahmed@gmail.com   "}
-
-    user_upload.remove_whitespaces(test)
-
-    assert test == expacted
-
-
-def test_set_email_lower_case__return_lower_case_email():
-    expacted = "ahmed@gmail.com"
-    test = {"email": "AHMED@gmaIL.com"}
-
-    assert user_upload.set_email_lower_case(test) == expacted
+if __name__ == "__main__":
+    unittest.main()
